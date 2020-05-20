7b9f12ee6c9fd823e3e2258f2599d0330e2d35b7
Andrielle Menegot <amenegot@thoughtworks.com>
2020-05-20 20:35:39 -0300

    Avoid magic numbers

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -23,27 +23,27 @@ class GildedRose {
     private void updateItem(Item item) {
         if (!item.name.equals("Aged Brie")
             && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) { // A qualidade de um item nunca é negativa
+            if (isQualityGreaterThanZero(item.quality)) { // A qualidade de um item nunca é negativa
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     decreasesQuality(item);
                 }
             }
             // "Aged Brie" e "Backstage passes" aumentam a qualidade à medida que seu valor SellIn se aproxima (todos os dias)
         } else {
-            if (item.quality < 50) { //A qualidade de um item nunca é superior que 50
+            if (isQualityLessThanFifty(item.quality)) { //A qualidade de um item nunca é superior que 50
                 incrementQuality(item);
 
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) { // "Backstage passes" aumenta qualidade em 2 quando faltam 10 dias
+                    if (isSellInLessThanEleven(item)) { // "Backstage passes" aumenta qualidade em 2 quando faltam 10 dias
                         //"Backstage passes" tem qualidade 49 max
-                        if (item.quality < 50) {
+                        if (isQualityLessThanFifty(item.quality)) {
                             //nao deveria ser 2?
                             incrementQuality(item);
                         }
                     }
 
-                    if (item.sellIn < 6) { // "Backstage passes" aumenta qualidade em 3 quando faltam 5 dias
-                        if (item.quality < 50) {
+                    if (isSellInLessThanSix(item)) { // "Backstage passes" aumenta qualidade em 3 quando faltam 5 dias
+                        if (isQualityLessThanFifty(item.quality)) {
                             //nao deveria ser 3?
                             incrementQuality(item);
                         }
@@ -52,31 +52,59 @@ class GildedRose {
             }
         }
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn -= 1;
+            decreasesSellIn(item);
         }
-        if (item.sellIn < 0) {
+        if (isSellInLessGreaterThanZero(item.sellIn)) {
             //Para itens que não são "Aged Brie"
             if (!item.name.equals("Aged Brie")) {
                 //Para itens que não são Backstage passes...
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
+                    if (isQualityGreaterThanZero(item.quality)) {
                         //Para itens que não são ("Sulfuras, Hand of Ragnaros" e tem qualidade maior que 0, degrada a qualidade
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                             decreasesQuality(item);
                         }
                     }
                 } else {
-                    // Qualidade cai pra 0 após o show
-                    item.quality = 0;
+                    item.quality = resetQuality(item.quality);
                 }
                 //Para itens que são "Aged Brie"
             } else {
-                // Se a qualidade do ítem for menor que 50 degrada a qualidade
-                if (item.quality < 50) {
+                if (isQualityLessThanFifty(item.quality)) {
                     incrementQuality(item);
                 }
             }
         }
+    }
+
+    private boolean isSellInLessThanEleven(Item item) {
+        return item.sellIn < 11;
+    }
+
+    private boolean isSellInLessThanSix(Item item) {
+        return item.sellIn < 6;
+    }
+
+    private boolean isSellInLessGreaterThanZero(int sellIn) {
+        return sellIn < 0;
+    }
+
+    private int resetQuality(int quality) {
+        quality = 0;
+
+        return quality;
+    }
+
+    private boolean isQualityGreaterThanZero(int quality) {
+        return quality > 0;
+    }
+
+    private boolean isQualityLessThanFifty(int quality) {
+        return quality < 50;
+    }
+
+    private void decreasesSellIn(Item item) {
+        item.sellIn -= 1;
     }
 
     private void decreasesQuality(Item item) {
